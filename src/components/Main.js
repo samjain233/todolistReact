3b94df68c246d85d4d0fde363e047a8a8a1d860f
Sambhav <sambhavjain233@gmail.com>
2022-12-03 12:26:17 +0530

    check button is not working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Task from "./Task";
-import TaskGenerator from "./TaskGenerator";
-import tasklist from './taskarray';
 
+let tasklist = [];
 
-const data = tasklist.map((item)=>{
+const data = tasklist.map((item) => {
   console.log(item);
-   return (
-    <Task {...item} />
-   )
+  return <Task {...item} />;
 });
 
 const Main = () => {
   const [task, setTask] = useState(data);
-  const addTask = (event)=>{
-    console.log("button is clicked");
-  }
+    const addTask = ()=>{
+      const newTask = document.getElementById("newTask").value;
+      const taskDate = document.getElementById("newDate").value;
+      const taskTime = document.getElementById("newTime").value;
+      const time = Date.parse(`${taskDate}T${taskTime}:00.000+05:30`);
+      const object = {
+        task : newTask ,
+        date : taskDate,
+        time : taskTime,
+        scriptTime : time,
+        isCompleted : false
+      }
+      tasklist.push(object);
+      const data = tasklist.map((item) => {
+        console.log(item);
+        return <Task {...item} />;
+      });
+      setTask(data);
+    }
   return (
     <>
       <div className="flex justify-center items-center flex-col">
         <div className="w-1/2 mt-8">
-          <TaskGenerator />
+          <div className="bg-blue-300 rounded shadow-lg mb-6">
+            <input
+              type="text"
+              className="p-2 my-3 mx-[3%] w-[94%] border-2 border-black rounded shadow-lg"
+              placeholder="Your New Task"
+              id="newTask"
+            />
+            <input
+              type="time"
+              className="p-2 my-3 mx-[3%] w-[44%] border-2 border-black rounded shadow-lg"
+              id="newTime"
+            />
+            <input
+              type="date"
+              className="p-2 my-3 mx-[3%] w-[44%] border-2 border-black rounded shadow-lg"
+              id="newDate"
+            />
+            <div className="button flex justify-center">
+              <button
+                className="p-3 px-5 m-2 bg-blue-600 rounded-full hover:bg-blue-700 shadow-lg"
+                onClick={addTask}
+              >
+                +
+              </button>
+            </div>
+          </div>
           {task}
         </div>
       </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,15 +2,16 @@ const Task = (props) => {
   return (
     <>
       <div className="bg-blue-200 mt-2 rounded p-1 shadow-lg">
-        <div className="flex flex-row">
-          <input type="checkbox" className="mx-1"></input>
-          <h1 className="mx-1 text-black">
-            sambhav jain is a good boy and he is the smartest pranneee on the
-            planet
-          </h1>
-          <button className="mx-1"><i className="fa fa-trash text-red-600" aria-hidden="true"></i></button>
+        <div className="flex flex-row w-full">
+          <input type="checkbox" className="mx-1" ></input>
+          <h1 className="mx-1 text-black">{props.task}</h1>
         </div>
-        <p className="text-sm mx-3 mt-3 mb-1">Due 10:30 , Today</p>
+          <button className="mx-1 float-right">
+            <i className="fa fa-trash text-red-600" aria-hidden="true"></i>
+          </button>
+        <p className="text-sm mx-3 mt-3 mb-1">
+          Due {props.time} , {props.date}
+        </p>
       </div>
     </>
   );
